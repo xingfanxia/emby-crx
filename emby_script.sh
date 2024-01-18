4c02414e33a498d62f54c6cf4701ef41194cff7e
Philo-Sophia <xingfanxia@gmail.com>
2024-01-18 04:26:49 -0800

    cleanup emby script

---
+++ emby_script.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+cd /var/packages/EmbyServer/target/system/dashboard-ui
+
+# 创建emby-crx目录并下载所需文件
+rm -rf emby-crx
+mkdir -p emby-crx
+wget https://raw.githubusercontent.com/xingfanxia/emby-crx/master/static/css/style.css -P emby-crx/
+wget https://raw.githubusercontent.com/xingfanxia/emby-crx/master/static/js/common-utils.js -P emby-crx/
+wget https://raw.githubusercontent.com/xingfanxia/emby-crx/master/static/js/jquery-3.6.0.min.js -P emby-crx/
+wget https://raw.githubusercontent.com/xingfanxia/emby-crx/master/static/js/md5.min.js -P emby-crx/
+wget https://raw.githubusercontent.com/xingfanxia/emby-crx/master/content/main.js -P emby-crx/
+
+chown -R emby:emby emby-crx/*
+chmod 644 emby-crx/*
+
+# 读取index.html文件内容
+content=$(cat index.html)
+
+# 检查index.html是否包含emby-crx
+if grep -q "emby-crx" index.html; then
+    echo "Index.html already contains emby-crx, skipping insertion."
+else
+    # 定义要插入的代码
+    code='<link rel="stylesheet" id="theme-css" href="emby-crx/style.css" type="text/css" media="all" />\n<script src="emby-crx/common-utils.js"></script>\n<script src="emby-crx/jquery-3.6.0.min.js"></script>\n<script src="emby-crx/md5.min.js"></script>\n<script src="emby-crx/main.js"></script>'
+
+    # 在</head>之前插入代码
+    new_content=$(echo -e "${content/<\/head>/$code<\/head>}")
+
+    # 将新内容写入index.html文件
+    echo -e "$new_content" > index.html
+fi
